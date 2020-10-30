3a0fff70c56c78fa9220967cbe28e872cd5fb777
MarkyJD <marky.dodson@gmail.com>
2020-10-30 21:52:13 +1100

    Added 'create post' area

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './Feed.css';
 import Post from './Post.js';
-
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import { CardContent, Button} from '@material-ui/core';
+import PersonIcon from '@material-ui/icons/Person';
 
 function Feed() {
     return (
         <div className="feed">
+            <div className="post-field">
+                <PersonIcon className="person-icon"/>
+                <TextField
+                    className="create-post"
+                    id="filled-full-width"
+                    label="Create Post"
+                    placeholder="Enter your message..."
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                />
+                <Button size="small" variant="contained" color="primary">
+                    Send
+                </Button>
+            </div>
+                
+            
             <Post title="211 Change Over" user="Mark" content="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."/>
             <Post title="211 Change Over" user="Mark" content="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."/>
             <Post title="211 Change Over" user="Mark" content="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."/>
