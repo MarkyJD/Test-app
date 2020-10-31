590df49e48dc7e17b51d815f61a45bf74e3e3ee1
MarkyJD <marky.dodson@gmail.com>
2020-10-31 14:16:21 +1100

    trying something different

---
+++ src/components/body/Body.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Body.css';
+import Card from '@material-ui/core/Card';
+
+function Body() {
+    return (
+        <div className="body">
+            
+        </div>
+    )
+}
+
+export default Body;
